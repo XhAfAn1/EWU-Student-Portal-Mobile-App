f0c68957964548760f1791db07d3e37d601df0ef
XhAfAn1 <116730623+XhAfAn1@users.noreply.github.com>
2024-12-25 21:09:58 +0600

    .

--- lib/main.dart
+++ lib/main.dart
@@ -153,6 +153,7 @@ class _HomeState extends State<Home> {
               children: [
 
                 Container(
+
                 child: IconButton(onPressed: (){}, icon: Badge.count(
                     count: 0,
                     padding: EdgeInsets.all(0.1),
